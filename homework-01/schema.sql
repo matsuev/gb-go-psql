7bd53fb10feeee54ac35c2197dfe27311b0255e5
Alex Matsuev <alex.matsuev@gmail.com>
2021-10-03 22:36:24 +0700

    Update schema.sql

--- homework-01/schema.sql
+++ homework-01/schema.sql
@@ -21,12 +21,13 @@
  *
  */
 
+\c golangdb
 
 -- Таблица школьников
 
 DROP TABLE IF EXISTS students CASCADE;
 CREATE TABLE students (
-   id INT GENERATED ALWAYS AS IDENTITY,   -- Идентификатор школьника
+   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,   -- Идентификатор школьника
    lname VARCHAR(50) NOT NULL,            -- Фамилия школьника
    fname VARCHAR(50) NOT NULL,            -- Имя школьника
    email VARCHAR(100) UNIQUE NOT NULL     -- Контактный адрес электронной почты
@@ -36,7 +37,7 @@ CREATE TABLE students (
 
 DROP TABLE IF EXISTS teachers CASCADE;
 CREATE TABLE teachers (
-   id INT GENERATED ALWAYS AS IDENTITY,   -- Идентификатор учителя
+   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,   -- Идентификатор учителя
    lname VARCHAR(50) NOT NULL,            -- Фамилия учителя
    fname VARCHAR(50) NOT NULL,            -- Имя учителя
    email VARCHAR(100) UNIQUE NOT NULL     -- Контактный адрес электронной почты
@@ -46,7 +47,7 @@ CREATE TABLE teachers (
 
 DROP TABLE IF EXISTS departments CASCADE;
 CREATE TABLE departments (
-   id INT GENERATED ALWAYS AS IDENTITY,   -- Идентификатор научного направления
+   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,   -- Идентификатор научного направления
    name VARCHAR(50),                      -- Название направления
    description TEXT                       -- Описание тематики направления
 );
@@ -55,8 +56,8 @@ CREATE TABLE departments (
 
 DROP TABLE IF EXISTS stages CASCADE;
 CREATE TABLE stages (
-   id INT GENERATED ALWAYS AS IDENTITY,   -- Идентификатор этапа
-   departmentId INTEGER NOT NULL,         -- Идентификатор направления
+   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,   -- Идентификатор этапа
+   departmentId INT NOT NULL,         -- Идентификатор направления
    name VARCHAR(250) NOT NULL,            -- Название этапа
    description TEXT,                      -- Описание тематики этапа
    FOREIGN KEY (departmentId) REFERENCES departments (id) ON UPDATE CASCADE ON DELETE RESTRICT
@@ -66,21 +67,20 @@ CREATE TABLE stages (
 
 DROP TABLE IF EXISTS levels CASCADE;
 CREATE TABLE levels (
-   id SERIAL PRIMARY KEY,              -- Идентификатор уровня сложности
-   name VARCHAR(100) NOT NULL,         -- Название уровня сложности
-   max_grade INTEGER NOT NULL          -- Максимально возможная оценка
+   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,   -- Идентификатор уровня сложности
+   name VARCHAR(100) NOT NULL,            -- Название уровня сложности
+   max_grade INT NOT NULL             -- Максимально возможная оценка
 );
 
 -- Таблица задач
-
 DROP TABLE IF EXISTS tasks CASCADE;
 CREATE TABLE tasks (
-   id SERIAL PRIMARY KEY,              -- Идентификатор задачи
-   levelId INTEGER NOT NULL,           -- Идентификатор уровня сложности
-   stageId INTEGER NOT NULL,           -- Идентификатор предметного этапа
-   title TEXT NOT NULL,                -- Заголовок задачи
-   content TEXT,                       -- Текст задачи
-   comments TEXT,                      -- Комментарии для проверяющего учителя
+   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,   -- Идентификатор задачи
+   levelId INT NOT NULL,              -- Идентификатор уровня сложности
+   stageId INT NOT NULL,              -- Идентификатор предметного этапа
+   title TEXT NOT NULL,                   -- Заголовок задачи
+   content TEXT,                          -- Текст задачи
+   comments TEXT,                         -- Комментарии для проверяющего учителя
    FOREIGN KEY (levelId) REFERENCES levels (id) ON UPDATE CASCADE ON DELETE RESTRICT,
    FOREIGN KEY (stageId) REFERENCES stages (id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
@@ -89,13 +89,13 @@ CREATE TABLE tasks (
 
 DROP TABLE IF EXISTS results CASCADE;
 CREATE TABLE results (
-   id SERIAL PRIMARY KEY,              -- Идентификатор результата
-   studentId INTEGER NOT NULL,         -- Идентификатор школьника
-   teacherId INTEGER NOT NULL,         -- Идентификатор проверяющего учителя
-   taskId INTEGER NOT NULL,            -- Идентификатор задачи
-   grade INTEGER NOT NULL,             -- Оценка
-   started_at TIMESTAMP NOT NULL,      -- Время начала решения задачи
-   finished_at TIMESTAMP NOT NULL,     -- Время окончания решения задачи
+   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,   -- Идентификатор результата
+   studentId INT NOT NULL,            -- Идентификатор школьника
+   teacherId INT NOT NULL,            -- Идентификатор проверяющего учителя
+   taskId INT NOT NULL,               -- Идентификатор задачи
+   grade INT NOT NULL,                -- Оценка
+   started_at TIMESTAMP NOT NULL,         -- Время начала решения задачи
+   finished_at TIMESTAMP NOT NULL,        -- Время окончания решения задачи
    FOREIGN KEY (studentId) REFERENCES students (id) ON UPDATE CASCADE ON DELETE RESTRICT,
    FOREIGN KEY (teacherId) REFERENCES teachers (id) ON UPDATE CASCADE ON DELETE RESTRICT,
    FOREIGN KEY (taskId) REFERENCES tasks (id) ON UPDATE CASCADE ON DELETE RESTRICT
